070abd9fc3c564779997a6c7e3e474f4e9bd443c
Daniel Waters <danwater1@gmail.com>
2018-05-10 15:18:57 -0700

    Added a search dashboard component

---
+++ src/components/SearchDashboard/SearchDashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchDashboard = () => (
+  <h1>Hi there</h1>
+);
+
+export default SearchDashboard;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import BookDashboard from './components/BookDashboard/BookDashboard';
 import './styles/styles.css';
 
 const jsx = (
   <BrowserRouter>
-    <Route exact path="/" component={BookDashboard} />
-    <Route path="/search" component={Search} />
+    <BookDashboard />
   </BrowserRouter>
 );
 
