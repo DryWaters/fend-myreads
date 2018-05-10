77bc2fc0da181d33d9ddd321193d117c601a2e38
Daniel Waters <danwater1@gmail.com>
2018-05-10 15:21:34 -0700

    Created routes for search and main page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import BookDashboard from './components/BookDashboard/BookDashboard';
+import SearchDashboard from './components/SearchDashboard/SearchDashboard';
 import './styles/styles.css';
 
 const jsx = (
   <BrowserRouter>
-    <BookDashboard />
+    <div>
+      <Route exact path="/" component={BookDashboard} />
+      <Route path="/search" component={SearchDashboard} />
+    </div>
+
   </BrowserRouter>
 );
 
