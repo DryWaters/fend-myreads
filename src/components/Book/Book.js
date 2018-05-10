3992c8c8d0ad37e1be9e78b3cc12932a429491ee
Daniel Waters <danwater1@gmail.com>
2018-05-10 11:58:52 -0700

    Book information is loaded dynamically now

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = props => (
+const Book = ({ details = {} }) => (
   <li>
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${details.imageLinks.smallThumbnail}")` }}></div>
         <div className="book-shelf-changer">
           <select>
             <option value="none" disabled>Move to...</option>
@@ -15,10 +16,16 @@ const Book = props => (
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{details.title}</div>
+      {details.authors.map(author => (
+        <div key={author} className="book-authors">{author}</div>
+      ))}
     </div>
   </li>
 );
 
 export default Book;
+
+Book.propTypes = {
+  details: PropTypes.object.isRequired,
+};
