7d398b4ee122b8917426e06ca60440604570f6b2
Daniel <danwater1@gmail.com>
2018-05-10 21:36:50 -0700

    Commented and cleaned up the Book Dashboard Component

--- src/components/BookDashboard/BookDashboard.js
+++ src/components/BookDashboard/BookDashboard.js
@@ -14,6 +14,8 @@ class BooksDashboard extends Component {
     this.moveBook = this.moveBook.bind(this);
   }
 
+  // fetch all books that the current user has from the BooksAPI
+  // when the application starts
   componentDidMount() {
     getAll()
       .then((data) => {
@@ -24,6 +26,9 @@ class BooksDashboard extends Component {
       });
   }
 
+  // Changes the book "shelf" status and pushes that information to the backend
+  // using the Books API.  If a book is set to "none" then it is removed from the
+  // local state so that the page will update correctly if no books are left
   moveBook(bookToChange, toWhere) {
     const { id } = bookToChange;
     update(bookToChange, toWhere);
@@ -31,6 +36,9 @@ class BooksDashboard extends Component {
       this.setState({ books: this.state.books.filter(book => book.id !== id) });
       return;
     }
+    // map over all books and return all parts of the book object, only
+    // modifing the shelf attribute that the backend uses to know which
+    // shelf to display on for the user
     const newBooks = this.state.books.map((book) => {
       if (book.id === id) {
         return {
@@ -43,11 +51,42 @@ class BooksDashboard extends Component {
     this.setState({ books: newBooks });
   }
 
-  shouldDisplayBookshelfs(bookTypes) {
+  // checks if should either show the no books component if the user has no
+  // books checked out, or all bookshelves if they have at least one.
+  // If there are books, then it divides them up by their "shelf" attribute
+  shouldDisplayBookshelves() {
     let jsx = '';
     if (this.state.books.length === 0) {
       jsx = <NoBooks />;
     } else {
+      const bookTypes = {
+        currentlyReading: {
+          title: 'Currently Reading',
+          books: [],
+        },
+        wantToRead: {
+          title: 'Want to Read',
+          books: [],
+        },
+        read: {
+          title: 'Read',
+          books: [],
+        },
+      };
+
+      this.state.books.forEach((book) => {
+        if (book.shelf === 'currentlyReading') {
+          bookTypes.currentlyReading.books.push(book);
+        } else if (book.shelf === 'wantToRead') {
+          bookTypes.wantToRead.books.push(book);
+        } else if (book.shelf === 'read') {
+          bookTypes.read.books.push(book);
+        }
+      });
+
+      // push down the moveBook function from this class down to the
+      // "BookShelf" component and then to the "Book" where it will
+      // be used
       jsx = Object.keys(bookTypes).map(shelfType => (
         <BookShelf
           key={shelfType}
@@ -62,39 +101,14 @@ class BooksDashboard extends Component {
   }
 
   render() {
-    const bookTypes = {
-      currentlyReading: {
-        title: 'Currently Reading',
-        books: [],
-      },
-      wantToRead: {
-        title: 'Want to Read',
-        books: [],
-      },
-      read: {
-        title: 'Read',
-        books: [],
-      },
-    };
-
-    this.state.books.forEach((book) => {
-      if (book.shelf === 'currentlyReading') {
-        bookTypes.currentlyReading.books.push(book);
-      } else if (book.shelf === 'wantToRead') {
-        bookTypes.wantToRead.books.push(book);
-      } else if (book.shelf === 'read') {
-        bookTypes.read.books.push(book);
-      }
-    });
-
     return (
       <div>
         <Header title="MyReads" />
         <div className="list-books-content">
-          {this.shouldDisplayBookshelfs(bookTypes)}
+          {this.shouldDisplayBookshelves()}
         </div>
         <div className="open-search">
-          <Link className="open-search a" href="/search" to="/search">Search </Link>
+          <Link className="open-search a" href="/search" to="/search">Search</Link>
         </div>
       </div>
     );
