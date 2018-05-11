7f9e76632e88ab6a9a762d7dc7fcf586adf5a36f
Daniel <danwater1@gmail.com>
2018-05-10 21:29:31 -0700

    Created two lines for no book code for readability

--- src/components/NoBooks/NoBooks.js
+++ src/components/NoBooks/NoBooks.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NoBooks = () => (
   <div className="search-books-results">
     <h2>No books!</h2>
-    <p>Visit the search page and type in one of the approved search terms.  See SEARCH_TERMS.md for more information.</p>
+    <p>Visit the search page and type in one of the approved search terms.</p>
+    <p>See SEARCH_TERMS.md for more information.</p>
   </div>
 );
 
